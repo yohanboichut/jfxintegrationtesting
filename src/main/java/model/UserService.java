9160c00a390d7b21346a77e20986d189e6de41ab
Integration continue <integrationcontinue@univ-orleans.fr>
2019-04-02 08:52:12 +0200

    Comments on UserService

--- src/main/java/model/UserService.java
+++ src/main/java/model/UserService.java
@@ -4,10 +4,25 @@ import java.util.Collection;
 
 public interface UserService {
 
+    /**
+     *
+     * @return the collection of all the registered users
+     */
 
     Collection<User> getUsers();
 
+
+    /**
+     * Delete of the datas the user whose the identifier is *id*
+     * @param id
+     */
     void deleteUser(long id);
 
+    /**
+     * Add a new user in the datas. A new identifier is generated and the data carried in *u*
+     * are copied.
+     * @param u
+     * @return
+     */
     long addUser(User u);
 }
